4dcb5c97905938d0d85e7c4ebfd34a2acb02dc8d
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-10-09 21:45:33 +0530

    adding all section with there style

--- mbachaiwala/src/App.js
+++ mbachaiwala/src/App.js
@@ -1,17 +1,25 @@
 
 import IntroVideo from './components/IntroVideo.jsx';
 import Section from './components/Section.jsx';
+import Footer from './components/Footer.jsx'
 import freshtopicImg from "./assets/academy.png";
 import freshtopicImg2 from "./assets/story.png";
 import tedTalksImg from "./assets/in-the-news.gif"
+import franchiseImg from "./assets/franchise.gif"
+import mapImg from "./assets/locations.png"
+import coursesImg from "./assets/image2.png"
+import albumImg from "./assets/mba-cares.gif"
+import baratImg from "./assets/image1.png"
+import chaiImg from "./assets/image3.png"
 import data from "./data/data.json"
 import './styles/App.scss';
 import './styles/intro.scss';
 import './styles/section.scss';
+import './styles/Footer.scss'
 
-const yellow="#fff100",pink="#ed1e79",white="#fff",brown="#6d30f";
+const yellow="#fff100",pink="#ed1e79",white="#fff",brown="#6d30f1";
 function App() {
-  const {freshTopic,freshTopic2,tedTalks}=data;
+  const {freshTopic,freshTopic2,tedTalks,franchise,map,courses,album,barat,chaiwala}=data;
   return (
     
     <>
@@ -19,6 +27,15 @@ function App() {
         <Section h3={freshTopic.heading} p={freshTopic.text} hasBtn={true} btntext={freshTopic.btn} imgSrc={freshtopicImg} backgroundColor={pink} headingColor={yellow} textColor={yellow} btnbgColor={yellow} btnColor={pink}/>
         <Section h3={freshTopic2.heading} p={freshTopic2.text} hasBtn={true} btntext={freshTopic2.btn} imgSrc={freshtopicImg2} backgroundColor={pink} headingColor={yellow} textColor={yellow} btnbgColor={yellow} btnColor={pink}/>
         <Section h3={tedTalks.heading} p={tedTalks.text} hasBtn={true} btntext={tedTalks.btn} imgSrc={tedTalksImg} backgroundColor={yellow} headingColor={pink} textColor={pink} btnbgColor={pink} btnColor={yellow}/>
+        <Section h3={franchise.heading} p={franchise.text} hasBtn={true} btntext={franchise.btn} imgSrc={franchiseImg} backgroundColor={white} headingColor={pink} textColor={brown} btnbgColor={brown} btnColor={yellow}/>
+        <Section h3={map.heading} p={map.text} hasBtn={false}  imgSrc={mapImg} backgroundColor={pink} headingColor={yellow} textColor={yellow}  />
+        <Section h3={courses.heading} p={courses.text} hasBtn={true} btntext={courses.btn} imgSrc={coursesImg} imgSize={"30%"}  backgroundColor={yellow} headingColor={pink} textColor={pink} btnbgColor={pink} btnColor={yellow}/>
+        <Section h3={album.heading} p={album.text} hasBtn={true} btntext={album.btn} imgSrc={albumImg}   backgroundColor={white} headingColor={pink} textColor={brown} btnbgColor={brown} btnColor={yellow}/>
+        <Section h3={barat.heading} p={barat.text} hasBtn={true} btntext={barat.btn} imgSrc={baratImg}   backgroundColor={pink} headingColor={yellow} textColor={yellow} btnbgColor={yellow} btnColor={pink}/>
+        <Section h3={chaiwala.heading} p={chaiwala.text} hasBtn={true} btntext={chaiwala.btn} imgSrc={chaiImg}   backgroundColor={white} headingColor={pink} textColor={brown} btnbgColor={brown} btnColor={yellow}/>
+
+
+      <Footer/>
     </>
   );
 }
---
+++ mbachaiwala/src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Footer() {
+  return (
+   <>
+        <footer>
+            <h1>Let's
+            <br />
+            #ConnectOnCutting?</h1>
+            <aside>
+                <a href="/"className='footerLinks'>Home</a>
+                <a href="/story"className='footerLinks'>Story</a>
+                <a href="/media"className='footerLinks'>Media</a>
+                <a href="/franchise"className='footerLinks'>Franchise</a>
+                <a href="/events"className='footerLinks'>Events</a>
+                <a href="/chaiwalacares"className='Chai Wala Cares'>Franchise</a>
+            </aside>
+            <div>
+                <h5>Email</h5>
+                <a href="mailto : pranjlachoudhary@gmail.com">pranjlachoudhary@gmail.com</a>
+                <a href="mailto : Info@mbachaiwala.com">  Info@mbachaiwala.com</a>
+            </div>
+            <div>
+                <h5>Phone</h5>
+                <a href="tel : +91 9571834213">9571834213</a>
+            </div>
+            <p>© COYPRIGHT 2022 MBA-CHAIWALA . </p>
+        </footer>
+        <div className='footer'></div>
+   </>
+  )
+}
+
+export default Footer
