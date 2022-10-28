acf9b266be3611be41a7e2fbfbc9113718d599bf
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-10-28 10:59:19 +0530

    creating Redux App

---
+++ reduxapp/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Navbar from './Navbar';
+import Shop from './Shop';
+function App() {
+  return (
+    <>
+    <Navbar/>
+   <div className='App'>
+      <h1>Redux App</h1>
+   </div>
+   <div className="container">
+      <Shop/>
+   </div>
+
+   
+   </>
+  );
+}
+
+export default App;
---
+++ reduxapp/src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h3>Deposit/Withdraw Money :-</h3>
+        <button className="btn btn-primary  mx-2">-</button>
+         Update Balance
+        <button className="btn btn-primary mx-2">+</button>
+    </div>
+  )
+}
+
+export default Shop
