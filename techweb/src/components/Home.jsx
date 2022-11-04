9950ec27bf89d1b18f5a267347780f8cc035df82
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-11-04 20:08:27 +0530

    structuring Home page

--- techweb/src/components/Home.jsx
+++ techweb/src/components/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import vg from "../assests/2.webp"
 
 const Home = () => {
   return (
-    <div>
-      <h1>home</h1>
+    <>
+      <div className='home'>
+      <main> 
+        <h1>home</h1>
+        <p>wellcome</p> 
+      </main>
     </div>
+    <div className="home2">
+      <img src={vg} alt="Graphics" />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente laboriosam harum tempora ullam excepturi blanditiis. Autem incidunt architecto cupiditate.</p>
+    </div>
+    </>
   )
 }
 
