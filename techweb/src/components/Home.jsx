53e17e516fbaa81fb984bce65bb0a8bbdfc269cd
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-11-14 22:47:53 +0530

    home3

--- techweb/src/components/Home.jsx
+++ techweb/src/components/Home.jsx
@@ -14,6 +14,11 @@ const Home = () => {
       <img src={vg} alt="Graphics" />
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente laboriosam harum tempora ullam excepturi blanditiis. Autem incidunt architecto cupiditate.</p>
     </div>
+    <div className="home3">
+      <div><h1>Who We Are!</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut laboriosam natus harum quidem, illum incidunt aperiam cumque reprehenderit quae molestiae blanditiis labore nostrum mollitia ducimus ut quisquam. Possimus ea aperiam quam. Ad dolorem enim accusantium tempore dolor, mollitia eum cupiditate fugit veritatis ex? Eius voluptates asperiores iusto nemo porro. Quaerat!</p>
+      </div>
+    </div>
     </>
   )
 }
