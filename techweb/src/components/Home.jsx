548f57a585d8cad492616add0260b7fdc2ed5dd3
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-11-14 23:04:10 +0530

    home4

--- techweb/src/components/Home.jsx
+++ techweb/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import vg from "../assests/2.webp"
-
+import {AiFillGoogleCircle,AiFillAmazonCircle,AiFillYoutube,AiFillInstagram} from "react-icons/ai"
 const Home = () => {
   return (
     <>
@@ -17,6 +17,38 @@ const Home = () => {
     <div className="home3">
       <div><h1>Who We Are!</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut laboriosam natus harum quidem, illum incidunt aperiam cumque reprehenderit quae molestiae blanditiis labore nostrum mollitia ducimus ut quisquam. Possimus ea aperiam quam. Ad dolorem enim accusantium tempore dolor, mollitia eum cupiditate fugit veritatis ex? Eius voluptates asperiores iusto nemo porro. Quaerat!</p>
+      </div>
+    </div>
+
+    <div className='home4'>
+      <div>
+      <h1>Brands</h1>
+      <article >
+        <div style={{
+          animationDelay: '0.3s',
+            }}>
+            <AiFillGoogleCircle />
+            <p>Google</p>
+        </div>
+        <div style={{
+          animationDelay: '0.5s',
+            }}>
+            <AiFillAmazonCircle />
+            <p>Amazon</p>
+        </div>
+        <div style={{
+          animationDelay: '0.7s',
+            }}>
+            <AiFillInstagram />
+            <p>Instagram</p>
+        </div>
+        <div style={{
+          animationDelay: '0.9s',
+            }}>
+            <AiFillYoutube />
+            <p>Youtube</p>
+        </div>
+      </article>
       </div>
     </div>
     </>
