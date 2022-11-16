1befb61acc873c4c4374d5b7a3fc5d7602a947f9
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-11-16 23:20:43 +0530

    contact page

--- techweb/src/App.js
+++ techweb/src/App.js
@@ -2,10 +2,13 @@ import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
 import Footer from './components/Footer';
+import Contact from './components/Contact';
+
 import './styles/App.scss';
 import './styles/Header.scss';
 import './styles/Home.scss';
 import './styles/Footer.scss';
+import './styles/Contact.scss';
 
 
 function App() {
